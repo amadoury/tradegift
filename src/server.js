309418c23b5367b6cebf4eb56855cdd444648a14
Amadou Oury Diallo <amadou7oury@gmail.com>
2024-04-12 01:25:20 +0200

    add login page

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const server = express();
 server.use(express.static('public'));
+server.use(express.urlencoded({extended : true}));
+server.set('view engine', 'ejs');
 
 
 server.get('/login', (req, res) => {
-    res.sendFile("login.html", {root:'public'});
+    res.sendFile("html/login.html", {root:'public'});
+});
+
+server.post('/login', (req, res) => {
+    console.log(req.body);
+    console.log("hello world");
 });
 
 server.listen(8080);
